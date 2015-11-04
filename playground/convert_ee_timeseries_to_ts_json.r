c9b2f19a140e9f10ae708762dda24aa7537d1e6c
jdbcode <jstnbraaten@gmail.com>
2015-11-03 16:14:34 -0800

    r script to convert a ee time series chart csv to ts json format. need to concatenate the two charts for tm5 and etm7 and change the format of the date to 00/00/00 in excel. used to create mock data to show all points in ts trajectory plot.

---
+++ playground/convert_ee_timeseries_to_ts_json.r
@@ -0,0 +1,32 @@
+
+
+library(rjson)
+
+csvFile="C:/Users/braatenj/Downloads/ee-chart.csv"
+outjsonfile = "C:/git_repos/timesync/playground/allValues.json"
+
+
+csv = read.csv(csvFile)
+csv = csv[complete.cases(csv),]
+colnames(csv)[1] = "Date"
+Date = as.Date(csv[,1],"%m/%d/%y")
+Year = format(Date, "%Y")
+
+df = cbind(Date,Year,csv[,2:ncol(csv)])
+
+df$Date = as.character(df$Date)
+df$Year = as.numeric(as.character(df$Year))
+
+
+for(i in 1:nrow(df)){
+  object = toJSON(as.list(df[i,]))
+  if(i == 1){
+      allobjects = paste('{"PlotID": 1, "Values": [',object,sep="")
+  } else if(i == nrow(df)){
+    allobjects = paste(allobjects,",",object,"]}",sep="")
+  } else {allobjects = paste(allobjects,",",object,sep="")}
+}
+
+write(allobjects,outjsonfile)
+
+
