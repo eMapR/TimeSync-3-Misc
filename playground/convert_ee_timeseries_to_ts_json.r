856bf6d5093ad5cfdd21a2c8e49353bd6fe83a74
jdbcode <jstnbraaten@gmail.com>
2015-11-05 16:40:32 -0800

    now shows a new window for the trajectory when expand button is clicked - code is redundant for this mock-up, will clean once settled on functionality. got rid of redundant code regarding forms.

--- playground/convert_ee_timeseries_to_ts_json.r
+++ playground/convert_ee_timeseries_to_ts_json.r
@@ -1,6 +1,7 @@
 
 
 library(rjson)
+library(lubridate)
 
 csvFile="C:/Users/braatenj/Downloads/ee-chart.csv"
 outjsonfile = "C:/git_repos/timesync/playground/allValues.json"
@@ -11,12 +12,13 @@ csv = csv[complete.cases(csv),]
 colnames(csv)[1] = "Date"
 Date = as.Date(csv[,1],"%m/%d/%y")
 Year = format(Date, "%Y")
+doy = yday(Date)
 
-df = cbind(Date,Year,csv[,2:ncol(csv)])
+df = cbind(Date,Year,doy,csv[,2:ncol(csv)])
 
 df$Date = as.character(df$Date)
 df$Year = as.numeric(as.character(df$Year))
-
+df$doy = as.numeric(as.character(df$doy))
 
 for(i in 1:nrow(df)){
   object = toJSON(as.list(df[i,]))
